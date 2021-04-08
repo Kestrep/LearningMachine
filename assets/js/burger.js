36b6d9140764a3c073581d540ce0a45d03459211
Kestrep <petot.alex@gmail.com>
2021-04-08 16:55:08 +0200

    Header and menu

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -3,8 +3,12 @@ function handleBurger() {
     burgers.forEach(burger => {
         burger.addEventListener('click', () => {
             burger.classList.toggle('active')
-            document.getElementById(burger.dataset.target).classList.toggle('menu-active')
+            // Dire au body de ne plus scroller
             document.querySelector('body').classList.toggle('overflow-hidden')
+
+            // If there is a burger, take the menu and put it in the main section
+            let menu = document.getElementById(burger.dataset.target)
+            menu.classList.toggle('active')
         })
     })
 }
