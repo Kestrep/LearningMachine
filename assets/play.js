9e7d05c829f83c6c60b1bd89d5fc1f74dcadb4e2
Kestrep <petot.alex@gmail.com>
2021-04-10 17:51:12 +0200

    Add rotating on card

--- assets/play.js
+++ assets/play.js
@@ -1,11 +1,20 @@
 function handleCard() {
     let currentCard = document.querySelector('.moving-card')
+    currentCard.style.transformOrigin = '50%, 50%'
+    currentCard.style.transition = '3ms ease-in'
 
-    let initialX, initialY, relativeX, relativeY
+    let initialX = 0
+    let initialY = 0
+    let relativeX = 0
+    let relativeY = 0
+    let translateX = 0
+    let translateY = 0
+    let rotatePercent = 0
     let initiate = false
 
     const littleThreshold = 50
-    const middleThreshold = 200
+    const middleThreshold = 100
+    const bigThreshold = 200
 
     currentCard.addEventListener('mousedown', e => {
         initialX = e.clientX
@@ -19,11 +28,27 @@ function handleCard() {
         relativeX = e.clientX-initialX
         relativeY = e.clientY-initialY
 
-        if(initiate && Math.abs(relativeX) < littleThreshold) {
-            currentCard.style.transform = `translate(${relativeX}px, ${relativeY}px)`
-        } else if (initiate && Math.abs(relativeX) > littleThreshold && Math.abs(relativeX) < middleThreshold) {
-            currentCard.style.transform = `translate(${(relativeX * 2) - (relativeX / Math.abs(relativeX)) * littleThreshold }px, ${relativeY}px)`
+        if(Math.abs(relativeX) < littleThreshold) {
+            translateX = relativeX
+            translateY = relativeY
+            rotatePercent = 0
+        } else if (Math.abs(relativeX) > littleThreshold && Math.abs(relativeX) < bigThreshold) {
+            direction = (relativeX / Math.abs(relativeX))
+            translateX = (relativeX * 2) - direction * littleThreshold
+            translateY = relativeY
+            rotatePercent = direction * (Math.abs(relativeX) - littleThreshold) / littleThreshold
+
+            /**
+             * Quand abs(relativeX) = littleThreshold, rotate = 0
+             * Quand abs(relativeX) = middleThreshold, rotate = 1
+             * 
+             * 
+             * 
+             */
+            console.log(rotatePercent)
         }
+
+        if(initiate) currentCard.style.transform = `translate(${translateX}px, ${translateY}px) rotateZ(${rotatePercent * 45}deg)`
         
     })
 
@@ -31,10 +56,10 @@ function handleCard() {
         initiate = false
 
         // Restore if threshold passed
-        if(Math.abs(relativeX) < littleThreshold) {
+        if(Math.abs(relativeX) < middleThreshold) {
             currentCard.style.transform = `translate(${0}px, ${0}px)`
         } else {
-            currentCard.style.transform = `translate(${(relativeX / Math.abs(relativeX) * 100)}vw, ${0}px)`
+            currentCard.style.transform = `translate(${(relativeX / Math.abs(relativeX) * 100)}vw, ${0}px) rotateZ(0)`
         }
     })
 
