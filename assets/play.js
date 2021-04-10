4b000a916be937326a4734db970a0d62d82e7e7d
Kestrep <petot.alex@gmail.com>
2021-04-10 18:14:15 +0200

    Fix some bug

--- assets/play.js
+++ assets/play.js
@@ -21,44 +21,40 @@ function handleCard() {
         initialY = e.clientY
         initiate = true
 
-        console.log('Hi')
+        console.log('DOWN || relative : ' + Math.abs(relativeX) + ' | client : ' + e.clientX + ' | initial : ' + initialX)
     });
 
     currentCard.addEventListener('mousemove', e => {
-        relativeX = e.clientX-initialX
-        relativeY = e.clientY-initialY
-
-        if(Math.abs(relativeX) < littleThreshold) {
-            translateX = relativeX
-            translateY = relativeY
-            rotatePercent = 0
-        } else if (Math.abs(relativeX) > littleThreshold && Math.abs(relativeX) < bigThreshold) {
-            direction = (relativeX / Math.abs(relativeX))
-            translateX = (relativeX * 2) - direction * littleThreshold
-            translateY = relativeY
-            rotatePercent = direction * (Math.abs(relativeX) - littleThreshold) / littleThreshold
-
-            /**
-             * Quand abs(relativeX) = littleThreshold, rotate = 0
-             * Quand abs(relativeX) = middleThreshold, rotate = 1
-             * 
-             * 
-             * 
-             */
-            console.log(rotatePercent)
+        if(initiate) {
+            relativeX = e.clientX-initialX
+            relativeY = e.clientY-initialY
+            
+            if(Math.abs(relativeX) < littleThreshold) {
+                translateX = relativeX
+                translateY = relativeY
+                rotatePercent = 0
+            } else if (Math.abs(relativeX) > littleThreshold && Math.abs(relativeX) < bigThreshold) {
+                direction = (relativeX / Math.abs(relativeX))
+                translateX = (relativeX * 2) - direction * littleThreshold
+                translateY = relativeY
+                rotatePercent = direction * (Math.abs(relativeX) - littleThreshold) / littleThreshold
+            }
+            currentCard.style.transform = `translate(${translateX}px, ${translateY}px) rotateZ(${rotatePercent * 45}deg)`
         }
 
-        if(initiate) currentCard.style.transform = `translate(${translateX}px, ${translateY}px) rotateZ(${rotatePercent * 45}deg)`
+        console.log('MOVE || relative : ' + Math.abs(relativeX) + ' | client : ' + e.clientX + ' | initial : ' + initialX)
         
     })
 
-    document.addEventListener('mouseup', () => {
+    document.addEventListener('mouseup', e => {
         initiate = false
 
         // Restore if threshold passed
         if(Math.abs(relativeX) < middleThreshold) {
             currentCard.style.transform = `translate(${0}px, ${0}px)`
         } else {
+            /* relativeX = e.clientX - initialX */
+            console.log('UP || relative : ' + Math.abs(relativeX) + ' | client : ' + e.clientX + ' | initial : ' + initialX)
             currentCard.style.transform = `translate(${(relativeX / Math.abs(relativeX) * 100)}vw, ${0}px) rotateZ(0)`
         }
     })
