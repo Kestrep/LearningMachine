cb30e910e06d014be2ac0c6aa4d1a01f81c30e0c
Kestrep <petot.alex@gmail.com>
2021-04-12 17:28:05 +0200

    Form design

--- src/Form/CardType.php
+++ src/Form/CardType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Card;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +13,15 @@ class CardType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('created_at')
-            ->add('front_maincontent')
-            ->add('front_subcontent')
-            ->add('back_main_content')
-            ->add('back_subcontent')
-            ->add('front_clue')
-            ->add('back_clue')
-            ->add('note')
-            ->add('stage')
+            // ->add('created_at')
+            ->add('front_maincontent', TextareaType::class)
+            ->add('front_subcontent', TextareaType::class)
+            ->add('back_main_content', TextareaType::class)
+            ->add('back_subcontent', TextareaType::class)
+            ->add('front_clue', TextareaType::class)
+            ->add('back_clue', TextareaType::class)
+            ->add('note', TextareaType::class)
+            // ->add('stage')
         ;
     }
 
