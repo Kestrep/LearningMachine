ef2dafccdb2c0cf2d979f20ecb19a9e723ded047
Kestrep <petot.alex@gmail.com>
2021-04-14 15:43:25 +0200

    Set aa user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -23,29 +23,43 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
-        
-        for ($i=0; $i < 30; $i++) { 
+
+        for ($i = 0; $i < 30; $i++) {
             $card = new Card();
 
             $card
                 ->setCreatedAt($faker->dateTimeBetween('-1 week', 'now'))
-            
-                ->setFrontMainContent($faker->sentence(mt_rand(2,3)))
-                ->setFrontSubcontent($faker->sentence(mt_rand(4,7)))
 
-                ->setBackMainContent($faker->sentence(mt_rand(2,3)))
-                ->setBackSubcontent($faker->sentence(mt_rand(4,7)))
-                
-                ->setFrontClue($faker->sentence(mt_rand(2,3)))
-                ->setBackClue($faker->sentence(mt_rand(2,3)))
+                ->setFrontMainContent($faker->sentence(mt_rand(2, 3)))
+                ->setFrontSubcontent($faker->sentence(mt_rand(4, 7)))
 
-                ->setNote($faker->sentence(mt_rand(8,12)))
-                ->setStage(mt_rand(0,7));
+                ->setBackMainContent($faker->sentence(mt_rand(2, 3)))
+                ->setBackSubcontent($faker->sentence(mt_rand(4, 7)))
+
+                ->setFrontClue($faker->sentence(mt_rand(2, 3)))
+                ->setBackClue($faker->sentence(mt_rand(2, 3)))
+
+                ->setNote($faker->sentence(mt_rand(8, 12)))
+                ->setStage(mt_rand(0, 7));
 
             $manager->persist($card);
         }
 
-        for ($i=0; $i < 30; $i++) { 
+        // Utilisateur standard
+        $user = new User();
+
+        $user
+            ->setEmail('aa@aa.aa')
+            ->setRoles(['ROLE_USER'])
+            ->setFirstname($faker->firstName())
+            ->setLastname($faker->lastName())
+            ->setRegisterAt($faker->dateTimeBetween('-4 week', '-1week'))
+            ->setLastConnectionAt($faker->dateTimeBetween('-1 week', 'now'))
+            ->setPassword($this->passwordEncoder->encodePassword($user, 'aaaa'));
+
+        $manager->persist($user);
+
+        for ($i = 0; $i < 30; $i++) {
             $user = new User();
 
             $user
@@ -54,9 +68,8 @@ class AppFixtures extends Fixture
                 ->setFirstname($faker->firstName())
                 ->setLastname($faker->lastName())
                 ->setRegisterAt($faker->dateTimeBetween('-4 week', '-1week'))
-                ->setLastConnectionAt($faker->dateTimeBetween('-1 week', 'now')) 
-                ->setPassword($this->passwordEncoder->encodePassword($user, 'pass'))
-                ;
+                ->setLastConnectionAt($faker->dateTimeBetween('-1 week', 'now'))
+                ->setPassword($this->passwordEncoder->encodePassword($user, 'pass'));
 
             $manager->persist($user);
         }
@@ -64,8 +77,3 @@ class AppFixtures extends Fixture
         $manager->flush();
     }
 }
-
-
-
-
-
